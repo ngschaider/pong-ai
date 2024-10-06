31cf025035432ff017a14244f8a11f2777a43f37
Niklas Gschaider <niklas.gschaider@gschaider-systems.at>
2024-10-06 05:14:08 +0200

    formatting

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -2,7 +2,6 @@ export const notEmpty = <T>(value: T|null|undefined): value is T  => {
     return value !== null && value !== undefined;
 }
 
-
 export const getCombinations = <T>(arr: T[]): [T, T][] => {
     return arr.flatMap((el, idx) => {
         return arr.slice(idx).map((w): [T, T] => [el, w]);
